367c3884e25552d3941dbb5cea156503217e29ba
gilliantuerah <gilliantuerah@gmail.com>
2021-02-05 13:50:48 +0700

    sdf

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,13 @@
-function Post({ date, image, title }) {
-    let { file, description } = image
+function Post({ Date, Image, Title }) {
+    let { file, description } = Image
   
     return (
       <div className="post">
         <img alt={description} src={`https:${file.url}`} />
         <div className="description">{description}</div>
         <div className="text">
-          <h2>{title}</h2>
-          <h3>{date.substring(0, 10)}</h3>
+          <h2>{Title}</h2>
+          <h3>{Date.substring(0, 10)}</h3>
         </div>
       </div>
     )
